53c86a1bd9e73dc8c9a0871a26fe585446969a1e
ZSheik2023 <zsheik374@gmail.com>
2023-06-16 11:37:49 +0530

    Navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from "../images/logo.png"
 import { Icon } from '@iconify/react';
 import "../css/Navbar.css"
 
+
 const Navbar = () => {
+
+  const [navToggle,setNavToggle] = useState(true)
+  const menuclick = () => {
+    if(navToggle){
+      setNavToggle(false)
+    }
+    else{
+      setNavToggle(true)
+    }
+  }
   return (
     <div>
       <div className='Navbar'>
@@ -18,9 +29,24 @@ const Navbar = () => {
             </ul>
         </div>
         <div className='Nav-btns'>
-            <button className='contact-btn'>Contact Us</button>
-            <Icon icon="icon-park:hamburger-button" className='Nav-menu' width="64" />
+              <button className='contact-btn'>Contact Us</button>
+            <div className='hamburger'>
+              <Icon icon={navToggle ? "icon-park:hamburger-button"  : "ep:close-bold"} className='menu-btn' width="64" onClick={menuclick} />
+            </div>
         </div>
+      </div>
+      <div className={navToggle ? "menu-container-hide "  : "menu-container-show"}>
+        <ul className='nav-menu'>
+          <li className='link-menu'>Home</li>
+          <div className='link-contain'>
+          <hr className='link-divide'/>
+          </div>
+          <li className='link-menu'>About</li>
+          <div className='link-contain'>
+          <hr className='link-divide'/>
+          </div>
+          <li className='link-menu'>Services</li>
+        </ul>
       </div>
     </div>
   )
