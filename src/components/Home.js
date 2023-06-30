34df4afbcadde54161204a59baf7cac9354a4523
ZSheik2023 <zsheik374@gmail.com>
2023-06-30 10:58:39 +0530

    half page has been successfullhy completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,20 +13,20 @@ export default function Home() {
     <div>
       <div className='hero'>
         <h1 className='hero-h'>
-            Experienced Craftsmen for all <br/> your Construction needs
+          Experienced Craftsmen for all <br /> your Construction needs
         </h1>
         <p className='hero-p'>
-            We provide comprehensive construction services to <br/> meet all of your needs. From residential remodelling <br/> to commercial construction.
+          We provide comprehensive construction services to <br /> meet all of your needs. From residential remodelling <br /> to commercial construction.
         </p>
         <div className='heroBtn-div'>
-            <button className='btn-2'>Learn more</button>
-            <button className='btn-3'>Our Services</button>
+          <button className='btn-2'>Learn more</button>
+          <button className='btn-3'>Our Services</button>
         </div>
       </div>
       <div className='blog-1'>
         <div>
           <h2 className='b1-h'>Our Vision</h2>
-          <h4 className='b1-sh'>At Apex, our vision is to be a leading player in the <br/> construction industry, known for delivering projects of <br/> exceptional quality and value.</h4>
+          <h4 className='b1-sh'>At Apex, our vision is to be a leading player in the <br /> construction industry, known for delivering projects of <br /> exceptional quality and value.</h4>
           <ul className='b1-list'>
             <li>Lead in construction quality and timely delivery</li>
             <li>Maintain high safety and environmental standards</li>
@@ -35,7 +35,46 @@ export default function Home() {
         </div>
         <img src={imgB1} alt="b1-img" className='b1-img' />
       </div>
-      <ServiceDiv title="Our Services" img1={imgS1} title1="Solar Installation" img2={imgS2} title2="Painting" img3={imgS3} title3="Interior Design"/>
+      <ServiceDiv title="Our Services" img1={imgS1} title1="Solar Installation" img2={imgS2} title2="Painting" img3={imgS3} title3="Interior Design" />
+      <div className='blog-2'>
+        <h2 className='b2-h'>Our Goals</h2>
+        <div className='b2-div'>
+          <div className='b2-box'>
+            <p className='b2-p'> Expertly shaping the future <br /> of construction, one <br /> project at a time.</p>
+          </div>
+          <div className='b2-box'>
+            <p className='b2-p'>Elevating construction <br /> through expert solutions <br /> and innovation.</p>
+          </div>
+          <div className='b2-box'>
+            <p className='b2-p'> Empowering excellence <br /> through integrity, innovation, <br /> and teamwork.</p>
+          </div>
+        </div>
+      </div>
+      <div className='blog-3'>
+        <h1 className='b3-h'>Industrial Achivements</h1>
+        <h2 className='b3-sh'>Trustworthy & Reliable</h2>
+        <p className='b3-p'>The Apex website provides a comprehensive overview of the construction industry, including current <br /> trends, market analysis, and innovative solutions. Repel is a one-stop destination for everything <br /> related to the industry.</p>
+          <div className='b3-div1'>
+            <div>
+              <h3 className='b3d-h'>130+</h3>
+              <p className='b3d-p'>Completed Projects</p>
+            </div>
+            <div>
+              <h3 className='b3d-h'>80+</h3>
+              <p className='b3d-p'>Satisfied Clients</p>
+            </div>
+          </div>
+          <div className='b3-div2'>
+            <div>
+              <h3 className='b3d-h'>15+</h3>
+              <p className='b3d-p'>On-going Projects</p>
+            </div>
+            <div>
+              <h3 className='b3d-h'>40+</h3>
+              <p className='b3d-p'>Government Projects</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
